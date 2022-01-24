8c3ed27a726f9e63c0ae791a5958c53bea579a62
web-ana <77341412+web-ana@users.noreply.github.com>
2022-01-25 00:58:59 +0400

    fixed bug in for cycle

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const bgColors = ["red", "yellow", "green", "blue"];
 let n = 0;
 let i = 0;
 for (i in bgColors) {
-  function changeColor() {
-    document.getElementById("colorName").innerHTML = bgColors[n];
-    document.body.style.background = bgColors[n];
-    ++n;
-  }
+  document.getElementById("colorName").innerHTML = bgColors[n];
+}
+function changeColor() {
+  document.body.style.background = bgColors[n];
+  ++n;
 }
